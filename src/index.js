5b1be07756e24553a8b6b1fdb42edeff0022bf62
Luis Antonio Peña Murillo <apmurillo99@hotmail.com>
2023-01-12 20:17:13 -0600

    feat: Se hace la estructura basica del back

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-console.log('*************');
-console.log('Hola mundo');
+require('dotenv').config();
+const express = require('express');
+const app = express();
+app.listen( process.env.PORT, () => 
+{
+    console.log('\x1b[34m ******************************************* \x1b[0m');
+    console.log('\x1b[34m ************Se levanto la API************** \x1b[0m');
+    console.log('\x1b[34m ******************************************* \x1b[0m');
+}) 
+
+// localhost:5000
+// localhost:5000/
+app.get('/', (req, res) => {
+    res.send('Hola mundo');
+})
+
+
+//Collections
+//Customers
